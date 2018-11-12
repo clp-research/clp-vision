629a96336e430098aa5c553e6d73bc93e0d09243
David Schlangen <david.schlangen@uni-bielefeld.de>
2018-11-13 00:03:59 +0100

    only keep 200 most similar for each image; massive reduction in size

--- Preproc/sim_preproc.py
+++ Preproc/sim_preproc.py
@@ -34,8 +34,25 @@ import numpy as np
 sys.path.append('../Utils')
 from utils import print_timestamped_message, code_icorpus
 
+MAX_ROWS = 30000  # max images to compare. beyond it gets too slow.
 
-def get_sim_mat(df, id_col='image_id', cat_col='cat', n_dims=False, max_row=None):
+
+def n_most_sim(obsims, row2imid, target_row, n=3):
+    '''
+    Utility function for looking up a similarity ranking in the
+    right batch and row, and then translating the batch column
+    (similar image) into original row number so that the image id
+    can be looked up in row2imid.
+    '''
+    batch_size = len(obsims[0])
+    in_batch = target_row // batch_size
+    translated_row = target_row - (batch_size * in_batch)
+    this_row = obsims[in_batch][translated_row][0:n+1]
+    return [row2imid[r + (batch_size * in_batch)] for r in this_row]
+
+
+def get_sim_mat(df, id_col='image_id', cat_col='cat',
+                n_dims=False, max_row=MAX_ROWS, n_most=200):
     print_timestamped_message('... compiling objects per image list')
     im2ob = defaultdict(list)
     df.apply(lambda row: im2ob[row[id_col]].append(row[cat_col]), axis=1)
@@ -52,10 +69,16 @@ def get_sim_mat(df, id_col='image_id', cat_col='cat', n_dims=False, max_row=None
         X = svd.fit_transform(X)
 
     print_timestamped_message('... and finally, computing similarities')
-    objsim_pdist = pdist(X[:max_row], 'cosine')
-    objsim = squareform(objsim_pdist)
 
-    return objsim, row2imid
+    objsim_most_sim_out = []
+    for i in range(0, len(X), max_row):
+        print_timestamped_message('... ... batch %d' % (i/max_row+1))
+        objsim_pdist = pdist(X[i:i+max_row], 'cosine')
+        objsim = squareform(objsim_pdist)
+        objsim_most_sim = np.apply_along_axis(lambda x: np.argsort(x)[:n_most], 1, objsim)
+        objsim_most_sim_out.append(objsim_most_sim)
+
+    return objsim_most_sim_out, row2imid
 
 
 # ======== MAIN =========
@@ -95,6 +118,7 @@ if __name__ == '__main__':
         bbdf_dir = '../Preproc/PreprocOut'
 
     for this_corp in args.corp:
+
         if this_corp == 'mscoco':
             print_timestamped_message('Computing sims for MSCOCO')
 
@@ -112,6 +136,7 @@ if __name__ == '__main__':
 
             print_timestamped_message('... compressing and writing to disk')
             np.savez_compressed(outfilename, sim_sq, row2imid)
+
         if this_corp == 'visgen':
             print_timestamped_message('Computing sims for Visual Genome')
 
@@ -128,7 +153,7 @@ if __name__ == '__main__':
             visgen_objdf = visgen_objdf[~visgen_objdf['syn'].isnull()]
 
             sim_sq, row2imid = get_sim_mat(visgen_objdf, cat_col='syn',
-                                           n_dims=50, max_row=40000)
+                                           n_dims=50, max_row=MAX_ROWS)
 
             print_timestamped_message('... compressing and writing to disk')
             np.savez_compressed(outfilename, sim_sq, row2imid)
