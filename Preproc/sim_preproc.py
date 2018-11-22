27181b44c3cb7c85fd66f43cfb4a144993d491e0
David Schlangen <david.schlangen@uni-bielefeld.de>
2018-11-22 11:10:41 +0100

    refactor, prepare for addition of visual similarity computation

--- Preproc/sim_preproc.py
+++ Preproc/sim_preproc.py
@@ -2,18 +2,14 @@
 '''
 Pre-compute similarity matrices for images.
 
-Currently implemented: Similarity based on annotation of objects present,
-for MSCOCO and visual genome.
+Object mode: Similarity based on annotation of objects present,
+for MSCOCO and visual genome. Each image is represented by a many-hot
+vector that notes the presence / annotation of a type of object, and the
+similarity computation is based on that.
 
+Visual mode: 
 
 TODO:
-x Looks like I can't compute similarity between all ~100k visgen
-  images; process gets killed. (Memory?). Should subsample to some
-  smaller number that works? Or do batches within which similarity is
-  computed? (So that for each image there is a set of distractors.)
-x Also, throw away columns?.. Ah, no.. Well. Could precompile a different
-  format. For each image, only keep the n most similar... Instead of keeping
-  the actual similarity matrix!
 '''
 
 from __future__ import division
@@ -35,7 +31,11 @@ import numpy as np
 sys.path.append('../Utils')
 from utils import print_timestamped_message, code_icorpus
 
-MAX_ROWS = 30000  # max images to compare. beyond it gets too slow.
+MAX_ROWS_OBJ = 30000  # max images to compare. beyond it gets too slow.
+MAX_ROWS_IMG = 10000  # max n to compare based on img features
+
+# The first features in the image feature Xs encode the region ID
+ID_FEATS = 3
 
 
 def load_imsim(path):
@@ -58,7 +58,7 @@ def n_most_sim(obsims, row2imid, target_row, n=3):
 
 
 def get_sim_mat(df, id_col='image_id', cat_col='cat',
-                n_dims=False, max_row=MAX_ROWS, n_most=200):
+                n_dims=False, max_row=MAX_ROWS_OBJ, n_most=200):
     print_timestamped_message('... compiling objects per image list')
     im2ob = defaultdict(list)
     df.apply(lambda row: im2ob[row[id_col]].append(row[cat_col]), axis=1)
@@ -76,6 +76,12 @@ def get_sim_mat(df, id_col='image_id', cat_col='cat',
 
     print_timestamped_message('... and finally, computing similarities')
 
+    objsim_most_sim_out = batched_similarity_computation(X, max_row, n_most)
+
+    return objsim_most_sim_out, row2imid
+
+
+def batched_similarity_computation(X, max_row, n_most):
     objsim_most_sim_out = []
     for i in range(0, len(X), max_row):
         print_timestamped_message('... ... batch %d' % (i/max_row+1))
@@ -84,13 +90,54 @@ def get_sim_mat(df, id_col='image_id', cat_col='cat',
         objsim_most_sim = np.apply_along_axis(lambda x: np.argsort(x)[:n_most], 1, objsim)
         objsim_most_sim_out.append(objsim_most_sim)
 
-    return objsim_most_sim_out, row2imid
+    return objsim_most_sim_out
+
+
+def run_objects_sim(bbdf_dir, this_corp):
+    if this_corp == 'mscoco':
+        print_timestamped_message('Computing sims for MSCOCO')
+
+        outfilename = bbdf_dir + '/mscoco_sim'
+        if isfile(outfilename + '.npz'):
+            print '%s exists. Will not overwrite. ABORTING.' % (outfilename + '.npz')
+            return
+
+        print_timestamped_message('... loading up bbdf')
+        mscoco_bbdf = pd.read_json(bbdf_dir + '/mscoco_bbdf.json.gz',
+                                   typ='frame',
+                                   orient='split', compression='gzip')
+
+        sim_sq, row2imid = get_sim_mat(mscoco_bbdf)
+
+        print_timestamped_message('... compressing and writing to disk')
+        np.savez_compressed(outfilename, sim_sq, row2imid)
+
+    if this_corp == 'visgen':
+        print_timestamped_message('Computing sims for Visual Genome')
+
+        outfilename = bbdf_dir + '/visgen_sim'
+        if isfile(outfilename + '.npz'):
+            print '%s exists. Will not overwrite. ABORTING.' % (outfilename + '.npz')
+            return
+
+        print_timestamped_message('... loading up bbdf')
+        visgen_objdf = pd.read_json(bbdf_dir + '/vgobjdf.json.gz',
+                                    typ='frame', orient='split',
+                                    compression='gzip')
+        visgen_objdf = visgen_objdf[~visgen_objdf['obj_id'].duplicated()]
+        visgen_objdf = visgen_objdf[~visgen_objdf['syn'].isnull()]
+
+        sim_sq, row2imid = get_sim_mat(visgen_objdf, cat_col='syn',
+                                       n_dims=50, max_row=MAX_ROWS_OBJ)
+
+        print_timestamped_message('... compressing and writing to disk')
+        np.savez_compressed(outfilename, sim_sq, row2imid)
 
 
 # ======== MAIN =========
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Use bbdfs to compute similarity btw images')
+        description='Use bbdfs or image features to compute similarity btw images')
     parser.add_argument('-c', '--config_file',
                         help='''
                         path to config file specifying data paths.
@@ -100,6 +147,15 @@ if __name__ == '__main__':
                         help='''
                         Where to look for the bbdf file.
                         default: '../Preproc/PreProcOut' ''')
+    parser.add_argument('-i', '--imfeat_dir',
+                        help='''
+                        Where to look for the image feature file.
+                        default: '../ExtractFeats/ExtractOut' ''')
+    parser.add_argument('mode',
+                        choices=['objects', 'visual'],
+                        help='''
+                        What to base the similarity computation on;
+                        annotated objects or visual similarity''')
     parser.add_argument('corp',
                         nargs='+',
                         choices=['mscoco', 'visgen'],
@@ -123,45 +179,15 @@ if __name__ == '__main__':
     else:
         bbdf_dir = '../Preproc/PreprocOut'
 
-    for this_corp in args.corp:
+    if args.imfeat_dir:
+        imfeat_dir = args.imfeat_dir
+    elif config.has_option('DSGV-PATHS', 'imfeat_dir'):
+        imfeat_dir = config.get('DSGV-PATHS', 'imfeat_dir')
+    else:
+        imfeat_dir = '../ExtractFeats/ExtractOut'
 
-        if this_corp == 'mscoco':
-            print_timestamped_message('Computing sims for MSCOCO')
-
-            outfilename = bbdf_dir + '/mscoco_sim'
-            if isfile(outfilename + '.npz'):
-                print '%s exists. Will not overwrite. ABORTING.' % (outfilename + '.npz')
-                break
-
-            print_timestamped_message('... loading up bbdf')
-            mscoco_bbdf = pd.read_json(bbdf_dir + '/mscoco_bbdf.json.gz',
-                                       typ='frame',
-                                       orient='split', compression='gzip')
-
-            sim_sq, row2imid = get_sim_mat(mscoco_bbdf)
-
-            print_timestamped_message('... compressing and writing to disk')
-            np.savez_compressed(outfilename, sim_sq, row2imid)
-
-        if this_corp == 'visgen':
-            print_timestamped_message('Computing sims for Visual Genome')
-
-            outfilename = bbdf_dir + '/visgen_sim'
-            if isfile(outfilename + '.npz'):
-                print '%s exists. Will not overwrite. ABORTING.' % (outfilename + '.npz')
-                break
-
-            print_timestamped_message('... loading up bbdf')
-            visgen_objdf = pd.read_json(bbdf_dir + '/vgobjdf.json.gz',
-                                        typ='frame', orient='split',
-                                        compression='gzip')
-            visgen_objdf = visgen_objdf[~visgen_objdf['obj_id'].duplicated()]
-            visgen_objdf = visgen_objdf[~visgen_objdf['syn'].isnull()]
-
-            sim_sq, row2imid = get_sim_mat(visgen_objdf, cat_col='syn',
-                                           n_dims=50, max_row=MAX_ROWS)
-
-            print_timestamped_message('... compressing and writing to disk')
-            np.savez_compressed(outfilename, sim_sq, row2imid)
+    if args.mode == 'objects':
+        for this_corp in args.corp:
+            run_objects_sim(bbdf_dir, this_corp)
 
     print_timestamped_message('Done!')
