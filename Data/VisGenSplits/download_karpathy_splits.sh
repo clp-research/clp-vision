e67fad9283f4ee07b89e32837acb54417b3e8208
David Schlangen <david.schlangen@uni-bielefeld.de>
2019-07-31 17:34:14 +0200

    add visgen splits

---
+++ Data/VisGenSplits/download_karpathy_splits.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/peteanderson80/bottom-up-attention/raw/master/data/genome/train.txt
+wget https://github.com/peteanderson80/bottom-up-attention/raw/master/data/genome/val.txt
+wget https://github.com/peteanderson80/bottom-up-attention/raw/master/data/genome/test.txt
